21dbe7a6b44b00a691bcd30eaeea599a91abba91
SuryaNarayananDev <suryavignesh6238@gmail.com>
2026-06-08 22:22:04 +0530

    fix : implement route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import MainContainer from './Components/MainContainer';
 import { useState } from 'react';
+import LoginPage from './Components/pages/LoginPage';
+import { Route, Routes } from 'react-router';
+import SignupPage from './Components/pages/SignupPage'; 
+import WelcomePage from './Components/pages/WelcomePage';
+import OnlineUsers from './Components/OnlineUsers';
+import CreateGroup from './Components/pages/CreateGroup';
+import WorkArea from './Components/WorkArea';
+import OnlineGroups from './Components/OnlineGroups';
 function App() {
   const [theme, setTheme] = useState(false);
   return (
     <div className={theme?"App-dark":"App-light"}>
-      <MainContainer theme={theme} setTheme={setTheme}/>
+      {/* <MainContainer theme={theme} setTheme={setTheme}/> */}
+      <Routes>
+        <Route path="/" element={<LoginPage theme={theme}/>} />
+        <Route path="/signup" element={<SignupPage theme={theme}/>} />
+        <Route path="app" element={<MainContainer theme={theme} setTheme={setTheme}/>}>
+          <Route path="welcome" element={<WelcomePage theme={theme}/>} />
+          <Route path="chat" element={<WorkArea theme={theme}/>} />
+          <Route path="users" element={<OnlineUsers theme={theme}/>} />
+          <Route path="groups" element={<OnlineGroups theme={theme}/>} />
+          <Route path="create-group" element={<CreateGroup theme={theme}/>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- frontend/src/Components/MainContainer.js
+++ frontend/src/Components/MainContainer.js
@@ -4,20 +4,20 @@ import SideBar from './SideBar'
 import WorkArea from './WorkArea'
 import WelcomePage from './pages/WelcomePage'
 import CreateGroup from './pages/CreateGroup'
-import SignupPage from './pages/SignupPage'
-import LoginPage from './pages/LoginPage'
+import OnlineGroups from './OnlineGroups'
 import OnlineUsers from './OnlineUsers'
+import { Outlet } from 'react-router'
 
 function MainContainer({ theme, setTheme }) {
   return (
     <div className="main-container">
       <SideBar theme={theme} setTheme={setTheme}/>
+      <Outlet/>
       {/* <WelcomePage theme={theme}/> */}
       {/* <WorkArea theme={theme}/> */}
       {/* <CreateGroup theme={theme}/> */}
-      {/* <SignupPage/> */}
-      {/* <LoginPage/> */}
-      <OnlineUsers theme={theme}/>
+      {/* <OnlineUsers theme={theme}/> */}
+      {/* <OnlineGroups theme={theme}/> */}
       
     </div>
   )
