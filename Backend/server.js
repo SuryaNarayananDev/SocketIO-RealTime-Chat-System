8d16a2f7f6612ab6ce9147534c21b9ef8d947480
SuryaNarayananDev <suryavignesh6238@gmail.com>
2026-06-11 11:52:04 +0530

    feat : inti backend server

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,13 @@
-console.log ("Server is running...");
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv .config();
+app.get('/', (req, res) => {
+    res.send("Stupid code Running ...... funny funny !");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port "+process.env.PORT);
+    // console.log("Database connection is successful");
+});
