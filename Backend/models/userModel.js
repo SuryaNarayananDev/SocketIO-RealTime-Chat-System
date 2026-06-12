2de991a2a19c400c0c1c9e276bafd61ac7ffe6d3
SuryaNarayananDev <suryavignesh6238@gmail.com>
2026-06-12 11:04:09 +0530

    fix : implement the Schema Model

---
+++ Backend/models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userModel = mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    mail:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type:String,
+        required: true,
+    }
+},
+{
+    timestamps: true
+})
+
+const User = mongoose.model("User", userModel);
+module.exports = User;
